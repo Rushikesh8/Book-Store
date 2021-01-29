a667bf751b28d0736aaddc3d75f4c93f5d204e6b
Rushikesh <rushikeshsakharwade@gmail.com>
2021-01-29 15:48:58 +0530

    Removed Image field from Category Model

--- store/models.py
+++ store/models.py
@@ -6,7 +6,6 @@ class Category(models.Model):
     name = models.CharField(max_length=250, unique=True)
     slug = models.SlugField(max_length=250, unique=True)
     description = models.TextField(blank=True)
-    image= models.ImageField(upload_to='category', blank=True)
 
     class Meta:
         ordering = ('name',)
